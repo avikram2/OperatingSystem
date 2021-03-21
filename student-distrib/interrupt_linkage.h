4e0907ae2efbcb7cfea965268850b2bf949d122a
Ayush Vikram <avikram2@illinois.edu>
2021-03-21 10:16:58 -0500

    Added ifndef ASM

--- student-distrib/interrupt_linkage.h
+++ student-distrib/interrupt_linkage.h
@@ -4,11 +4,15 @@
 #include "lib.h"
 #include "interrupts.h"
 
+#ifndef ASM
+
+
 //assembly linkage function for keyboard interrupt
 extern void keyboard_wrapper();
 
 //assembly linkage for RTC interrupt
 extern void rtc_wrapper();
 
+#endif //ASM
 
 #endif //interrupt_linkage
