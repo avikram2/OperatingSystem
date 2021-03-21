49e4dc3cdc918bee7aafae9fbad8a2eaa011cad2
Colin Wallace <colinwallace@Colins-MacBook-Pro.local>
2021-03-21 17:07:44 -0500

    Adding in RTC initialization files

---
+++ student-distrib/rtc.c
@@ -0,0 +1,19 @@
+
+#include "rtc.h"
+#include "i8259.h"
+/*  Description: Initialize the RTC to IRQ8.
+ *  Input: None
+ *  Output: None
+ *  Effect: initialize RTC and set bitmask for PIC
+ */
+void init_rtc(){
+    //From OSDev
+    cli();
+    outb(REG_B, REG_SELECT);      //Select register B (0xB), and disable NMI (0x80)
+    char prev = inb(DATA_PORT); //Read the current value of register B
+    outb(REG_B, REG_SELECT);      //Set the index again (a read will reset the index to register D)
+    outb(prev | 0x40, DATA_PORT); //
+    sti();
+    enable_irq(8); //enable RTC interrupts
+    return;
+}
---
+++ student-distrib/rtc.h
@@ -0,0 +1,16 @@
+#ifndef _RTC_H
+#define _RTC_H
+
+#include "types.h"
+
+
+#define REG_SELECT      0x70
+#define DATA_PORT       0x71
+#define REG_A           0x8A
+#define REG_B           0x8B
+
+
+
+extern void init_rtc();
+
+#endif
