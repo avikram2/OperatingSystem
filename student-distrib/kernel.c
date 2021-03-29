633e9c5a96cadd7c74867b0020e65991a5d61f49
jwwalls2 <jwwalls2@illinois.edu>
2021-03-29 16:41:03 +0000

    Uncommenting init_rtc() in kernel.c

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -150,7 +150,7 @@ void entry(unsigned long magic, unsigned long addr) {
      * PIC, any other initialization stuff... */
     //init keyboard
     keyboard_init();
-  //init_rtc(); 
+    init_rtc(); 
 //init the RTC and enable RTC interrupts
     /* Enable interrupts */
     /* Do not enable the following until after you have set up your
