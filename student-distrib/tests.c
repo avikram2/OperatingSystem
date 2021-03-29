37032b6fcdf369caff9a631abd3e0e6711914a0b
Ayush Vikram <avikram2@illinois.edu>
2021-03-29 14:52:31 -0500

    Editing testing

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -152,12 +152,12 @@ int terminal_driver_test(){
 	terminal_open(NULL); //open terminal
 	clear();
 	update_cursor(ORIGIN_CURSOR, ORIGIN_CURSOR);
-	uint8_t* string = "\0\0\0\0\0\0\0Hello, what is your name";
-	uint8_t * nullbuf = "\0\0\0\0\0a";
-	int j;
-	for (j = 0; j < 6; ++j){
-		putc(nullbuf[j]);
-	}
+	uint8_t* string = "Hello, what is your name \n";
+	// uint8_t * nullbuf = "\0\0\0\0\0a";
+	// int j;
+	// for (j = 0; j < 6; ++j){
+	// 	putc(nullbuf[j])
+	// }
 	terminal_write(0, string, strlen(string));
 	uint8_t buff[BUFFER_SIZE];
 	int retval = terminal_read(0, buff, BUFFER_SIZE-1);
