2f64cc61530537ebc89cb87b707dc4bb953f150b
Walls <jwwalls2@illinois.edu>
2021-03-29 10:58:47 -0500

    RTC Virtualization Comments

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -128,15 +128,17 @@ int video_memory_access_test(){
 	return PASS;
 }
 
-//testing to see if accesses in video memory are permitted, in order to test if video memory paging set up properly
-//covers paging, video memory paging
-//files: paging.c
+//testing to see if we can egenrate interrupts with the rtc driver
+//covers rtc interrupts, the rtc driver
+//should wait for 10 seconds, regardless of the frequency set
+//files: rtc_driver.c/h
 int rtc_test(){
+	int frequency = 2;
 	int count = 0;
 	int rtc_instance;
 	rtc_open(&rtc_instance);
-	rtc_write(rtc_instance, 512);
-	for(count = 0;count<5120;count++)
+	rtc_write(rtc_instance, frequency);
+	for(count = 0;count<10 * frequency;count++)
 	{
 		rtc_read(rtc_instance);
 	}
